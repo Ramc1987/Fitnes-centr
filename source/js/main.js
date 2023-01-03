9507e2e51c17acce6f7656b5308de632121b97ce
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2023-01-03 23:17:24 +0300

    Finish work desctop version gym in the section

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {videoEnabled} from './modules/video-play';
 
 // ---------------------------------
 
@@ -9,6 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  videoEnabled();
 
   // Modules
   // ---------------------------------
