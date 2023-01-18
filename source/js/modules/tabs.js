fb669043f6ca12a8e6f0473ee758b470e4587af1
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2023-01-18 17:46:57 +0300

    Add hover and focus in the project

--- source/js/modules/tabs.js
+++ source/js/modules/tabs.js
@@ -1,6 +1,6 @@
 const activatesTabs = () => {
   const tabs = document.querySelectorAll('.tabs__button');
-  const tabsContent = document.querySelectorAll('.tab__list');
+  const tabsContent = document.querySelectorAll('.tabs__list');
 
   if (tabs) {
     tabs.forEach((tab) => {
@@ -8,10 +8,10 @@ const activatesTabs = () => {
         const target = document.querySelector(tab.dataset.target);
 
         tabsContent.forEach((item) => {
-          item.classList.remove('tab__list--active');
+          item.classList.remove('tabs__list--active');
         });
 
-        target.classList.add('tab__list--active');
+        target.classList.add('tabs__list--active');
 
         tabs.forEach((tabitem) => {
           tabitem.classList.remove('tabs__button--active');
